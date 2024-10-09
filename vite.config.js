67992df498f610e78daaf1aa3aa6e37dc874c3c5
Voodoo <msl2002@yandex.ru>
2024-10-09 14:52:15 +0300

    📃 Add github pages

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: 'random-quote-machine',
   resolve: {
     alias: {
       '@components': '/src/components',
